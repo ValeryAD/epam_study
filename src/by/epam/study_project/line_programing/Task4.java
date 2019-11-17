59e76ba4c4c26218c3dcb2dca3c1f1acbfbb64e9
Valery Doroshko <DoroshkoValery@gmail.com>
2019-11-17 16:48:59 +0300

    amend Task4

--- src/by/epam/study_project/line_programing/Task4.java
+++ src/by/epam/study_project/line_programing/Task4.java
@@ -10,29 +10,34 @@ public class Task4 {
     public static void main(String[] args) {
 
         final int CAPACITY = 1000;
-        final double MAX_VALUE = 999.999;
-        final double MIN_VALUE = -999.999;
 
         double r = 0;
         double result = 0;
         Scanner sc = new Scanner(System.in);
-        final String INPUT_REQUEST = "Please, type in double value \nbetween 999,999 and -999,999";
+        final String inputRequest = "Please, type in double value \nbetween 999,999 and -999,999";
+        final String wrongInputMessage = "Wrong input";
         boolean isNotCorrectValue = false;
 
         do {
-            System.out.println(INPUT_REQUEST);
+            if (isNotCorrectValue) {
+                System.out.println(wrongInputMessage);
+            }
+            System.out.println(inputRequest);
             while (!sc.hasNextDouble()) {
                 sc.next();
-                System.out.println(INPUT_REQUEST);
+                System.out.println(wrongInputMessage + "\n" + inputRequest);
             }
 
             r = sc.nextDouble();
-            isNotCorrectValue = (r > MAX_VALUE || r < MIN_VALUE);
+            isNotCorrectValue = (r >= CAPACITY || r <= -CAPACITY);
         } while (isNotCorrectValue);
 
 
         int intValue = (int) r;
-        int reminder = (int) (r * CAPACITY - intValue * CAPACITY);
+        //int reminder = (int) (r % intValue * CAPACITY); //это выражение лаконичней, но иногда из-за double целая часть
+        //результата получается на 1 меньше
+        int reminder = (int) (r * 1000 - intValue * 1000); // так всегда правильно
+
         result = reminder + ((double) intValue / CAPACITY);
         System.out.print(result);
     }
