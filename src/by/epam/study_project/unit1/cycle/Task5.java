d91d22183c9ef4733b561e7d0bab857957547260
Valery Doroshko <DoroshkoValery@gmail.com>
2019-11-19 22:48:55 +0300

    add Task5 in cycle

---
+++ src/by/epam/study_project/unit1/cycle/Task5.java
@@ -0,0 +1,43 @@
+package by.epam.study_project.unit1.cycle;
+
+        /*5. Даны числовой ряд и некоторое число е. Найти сумму тех членов ряда, модуль которых больше или равен
+        заданному е. Общий член ряда имеет вид:*/
+
+import java.util.Scanner;
+
+import static java.lang.Math.pow;
+
+public class Task5 {
+    public static void main(String[] args) {
+        int n = 0;
+        double e = 0;
+        double an = 0;
+        double sum = 0;
+        Scanner sc = new Scanner(System.in);
+        final String eInputRequest = "Введите число \'e\'";
+        final String nInputRequest = "Введите число \'n\'";
+        final String wrongInput = "Неверный ввод";
+
+        System.out.println(eInputRequest);
+        while (!sc.hasNextDouble()) {
+            sc.next();
+            System.out.println(wrongInput + "\n" + eInputRequest);
+        }
+        e = sc.nextDouble();
+
+        System.out.println(nInputRequest);
+        while (!sc.hasNextInt()) {
+            sc.next();
+            System.out.println(wrongInput + "\n" + nInputRequest);
+        }
+        n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            an = Math.abs(1 / pow(2, i) + 1 / pow(3, i));
+            if(an > e){
+                sum += an;
+            }
+        }
+        System.out.println(sum);
+    }
+}
