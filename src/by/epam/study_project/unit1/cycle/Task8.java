4489baa6c665f5f03fcfdabed20990c726d1fca0
Valery Doroshko <DoroshkoValery@gmail.com>
2019-11-20 21:20:10 +0300

    finish task 8 in cycle

--- src/by/epam/study_project/unit1/cycle/Task8.java
+++ src/by/epam/study_project/unit1/cycle/Task8.java
@@ -2,19 +2,41 @@ package by.epam.study_project.unit1.cycle;
 
 //8. Даны два числа. Определить цифры, входящие в запись как первого так и второго числа.
 
+import java.util.Scanner;
+
 import static java.lang.Math.abs;
 
 public class Task8 {
     public static void main(String[] args) {
-        int x = 7;
-        int y = 1356;
+        int x = 0;
+        int y = 0;
         int tempY = 0;
         int reminderX = 0;
         int reminderY = 0;
         final int base = 10;
         StringBuilder answer = new StringBuilder();
+        Scanner sc = new Scanner(System.in);
+        final String inputRequest1 = "Введите первое число";
+        final String inputRequest2 = "Введите второе число";
+        final String wrongInput = "Неверный ввод";
 
+        //getting user data
 
+        System.out.println(inputRequest1);
+        while (!sc.hasNextInt()) {
+            sc.next();
+            System.out.println(wrongInput + "\n" + inputRequest1);
+        }
+        x = abs(sc.nextInt());
+
+        System.out.println(inputRequest2);
+        while (!sc.hasNextInt()) {
+            sc.next();
+            System.out.println(wrongInput + "\n" + inputRequest2);
+        }
+        y = abs(sc.nextInt());
+
+        //calculation
         do {
             reminderX = x % base;
             tempY = y;
@@ -25,15 +47,15 @@ public class Task8 {
                     answer.append(reminderY + ", ");
                 }
                 tempY /= base;
-            } while (abs(tempY) >= 1);
+            } while (tempY >= 1);
 
             x /= base;
-        } while (abs(x) >= 1);
+        } while (x >= 1);
 
-
-        if (answer.length()>2) {
-            answer.delete(answer.length()-2, answer.length());
+        if (answer.length() > 2) {
+            answer.delete(answer.length() - 2, answer.length());
         }
+
         System.out.println(answer);
     }
 
