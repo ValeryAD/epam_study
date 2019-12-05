20a58f4b9b63d62c7d5c1189991c6d1aeb579f37
Valery Doroshko <DoroshkoValery@gmail.com>
2019-12-05 16:29:24 +0300

    add unit2.mult_dim_arr.Task1

--- src/by/epam/study_project/unit2/mult_dim_arr/Task1.java
+++ src/by/epam/study_project/unit2/mult_dim_arr/Task1.java
@@ -1,8 +1,36 @@
 package by.epam.study_project.unit2.mult_dim_arr;
 
-/**
- * Created by Администратор on 05.12.2019.
- */
+//1. Дана матрица. Вывести на экран все нечетные столбцы, у которых первый элемент больше последнего.
+
 public class Task1 {
-    
+    public static void main(String[] args) {
+        final int n = 5;
+        //int[][] matrix = new int[n][n];
+
+       /* for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++){
+                matrix[i][j] = (int)(Math.random() * 10 + 1);
+            }
+        }*/
+
+        int[][] matrix = {{1, 5, 1, 2, 2, 8}, {2, 2, 2, 2, 2, 2}, {3, 3, 3, 3, 3, 3,}};
+        //TODO нечетные?
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                boolean odd = i % 2 != 0;
+                boolean biggerFirstEl = matrix[i][0] > matrix[i][matrix[i].length - 1];
+                if (odd) {
+                    System.out.printf("%3d", matrix[i][j]);
+                }
+            }
+            System.out.println();
+        }
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.printf("%3d", matrix[i][j]);
+            }
+            System.out.println();
+        }
+    }
 }
